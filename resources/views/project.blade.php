0f67dd6cbe0313109c12392a34725a270ab80375
glbdmn <gabrieledamiani@hotmail.it>
2023-06-19 17:58:01 +0200

    fine esercizio

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,31 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@extends('layouts.app')
 
-</head>
-<body>
+@section('content')
     <h1 class="container">tabella personale</h1>
 
     <div class="container">
-    <div class="row">
-        @foreach ($projects as $elem)
-          <div class="col-2">
-            <div class="card">
-                <div class="card-body">
-                        <h4 class="card-title">{{$elem->title}} </h4>
-                    </a>
-                    
-                    <p class="card-text">{{ $elem->content }}</p>
+        <div class="row">
+                @foreach ($projects as $elem)
+                <div class="col-2">
+                    <div class="card">
+                        <div class="card-body">
+                                <h4 class="card-title">{{$elem->title}} </h4>
+                            </a>
+                            
+                            <p class="card-text">{{ $elem->content }}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-          </div>
 
-        @endforeach
-    </div>
-</div> 
-
-</body>
-</html>
+                @endforeach
+        </div>
+    </div> 
+@endsection  
